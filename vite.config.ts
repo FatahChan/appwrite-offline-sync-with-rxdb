e5442188e7e61bd26ad416cf66f2fd4d337a3e28
FatahChan <ahmadfathallah89@gmail.com>
2024-10-13 22:37:36 +0300

    feat: added pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["**/*"],
+      registerType: "autoUpdate",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "Appwrite RxDb Example",
+        short_name: "Appwrite RxDb",
+        description: "Example project for Appwrite and RxDb",
+        theme_color: "#ffffff",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
   esbuild: {
     supported: {
       "top-level-await": true, //browsers can handle top-level-await features
